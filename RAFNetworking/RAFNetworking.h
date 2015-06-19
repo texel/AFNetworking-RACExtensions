09f7fc81f5f42f93cf09adaabb2a7f8bce515569
Leigh Caplan <texel1@gmail.com>
2015-06-18 17:34:56 -0700

    Add a Cartfile and framework

---
+++ RAFNetworking/RAFNetworking.h
@@ -0,0 +1,19 @@
+//
+//  RAFNetworking.h
+//  RAFNetworking
+//
+//  Created by Leigh Caplan on 6/18/15.
+//  Copyright (c) 2015 CodaFi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RAFNetworking.
+FOUNDATION_EXPORT double RAFNetworkingVersionNumber;
+
+//! Project version string for RAFNetworking.
+FOUNDATION_EXPORT const unsigned char RAFNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RAFNetworking/PublicHeader.h>
+
+
